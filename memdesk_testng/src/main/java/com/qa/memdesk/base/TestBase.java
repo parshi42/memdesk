34791134fbaa9587a4e76ebabd57f6ed724d5e9d
parshi42 <prashanthnamani137@gmail.com>
2019-11-01 15:56:21 +0530

    removing comments

--- memdesk_testng/src/main/java/com/qa/memdesk/base/TestBase.java
+++ memdesk_testng/src/main/java/com/qa/memdesk/base/TestBase.java
@@ -46,7 +46,6 @@ public class TestBase {
 		
 		if(browserName.equals("chrome")){			
 			System.setProperty("webdriver.chrome.driver","D://memdesk_testng//chromedriver.exe");
-			//Adding Incognito Mode
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("Incognito");
 			DesiredCapabilities cap = DesiredCapabilities.chrome();
